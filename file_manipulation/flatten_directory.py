61171c9ca6e1f86a56bfa66c8dd1f68ee9ed9be9
Vedrana Andersen Dahl <vedranaa@gmail.com>
2022-10-01 15:13:21 +0200

    Update flatten_directory.py

--- file_manipulation/flatten_directory.py
+++ file_manipulation/flatten_directory.py
@@ -11,9 +11,8 @@ import shutil
 
 
 def get_extensions(description):
-    if description == 'all':
-        extensions = True
-    elif description == 'photo':
+    
+    if description == 'photo':
         extensions = ['jpeg', 'jpg', 'png', 'gif', 'bmp',
                     'avi', 'qt', 'mov', 'mp4', 'm4p', 'mpg', 'mpeg']
     elif description == 'image':
@@ -21,6 +20,15 @@ def get_extensions(description):
     else:
         extensions = description
     return extensions
+
+
+def printlist(ls):
+    for l in ls:
+        if isinstance(l, tuple):
+            print(l[0])
+            print('  ->  ' + l[1])
+        else:
+            print(l)
 
 
 def copy_flattened(root, destination, copy_all='all', ask_first=False):
@@ -39,11 +47,11 @@ def copy_flattened(root, destination, copy_all='all', ask_first=False):
     ask_first : Bool, optional
         Whether to ask for confirmation before copying. The default is False.
         !!! SPYDER 5.1 HAS PROBLEMS WITH FUNCTION input. DON'T USE ASK_FIRST
-        !!! IF RUNNING FROM AFFECTED SPYDER.
+        !!! IF RUNNING FROM THE AFFECTED SPYDER.
 
     Returns
     -------
-    copy : list of tupples
+    copy : list of tuples
         Files (to be) copied.
     skip : list of stringe
         Files (to be) skipped.
@@ -64,7 +72,6 @@ def copy_flattened(root, destination, copy_all='all', ask_first=False):
     else:
         print('Proceeding with copy.')
         for c in copy:
-            #print(c[1])
             dst = os.path.join(destination, c[1])
             shutil.copyfile(c[0], dst)
     
@@ -72,21 +79,28 @@ def copy_flattened(root, destination, copy_all='all', ask_first=False):
     return copy, skip, processed
 
 
-def flatten(foldername, copy_all='all', prefix='', copy=[], skip=[], processed=[]):
+def flatten(foldername, copy_all='all', prefix=''):
 
+    copy=[]
+    skip=[]
+    processed=[]
+    
     processed.append(foldername)    
     files = glob.glob(foldername + '/*')
     L = len(foldername) + 1  # + 1 due to '/' 
     extensions = get_extensions(copy_all)
     
     for f in files:
+        n = prefix + '_' + f[L:]
         if os.path.isdir(f):
-            copy, skip, processed = flatten(f, copy_all, prefix + '_' + f[L:])
+            c, s, p = flatten(f, copy_all, n)
+            copy = copy + c
+            skip = skip + s
+            processed = processed + p
         else:
             fn, ext = os.path.splitext(f)
-            # First part evaluates to false if string or list.
-            if  (extensions==True) or (ext[1:].lower() in extensions):  
-                n = prefix + '_' + f[L:]
+            ext = ext[1:].lower()
+            if  (extensions=='all') or (ext==extensions) or (ext in extensions):  
                 copy.append((f, n[1:]))
             else:
                 skip.append(f)
@@ -108,14 +122,26 @@ def flatten(foldername, copy_all='all', prefix='', copy=[], skip=[], processed=[
 #         f.write(l + '\n')
 
 #%%
+test_folder = '/Users/VAND/Documents/PROJECTS/Colorsphere'
 
-photos = '/Volumes/Toshiba1/PICTURES/PHOTOS/2006'
-flat = '/Users/vand/Documents/_PHOTOS_FLATTENED/2006'
-copy, skip, processed = flatten(photos, copy_all='photo')
+copy, skip, processed = flatten(test_folder, 'image')
+print('*** COPY ***')
+printlist(copy)
+print('*** SKIP ***')
+printlist(skip)
+print('*** PROCESSED ***')
+printlist(processed)
 
 #%%
-check = [s for s in skip if s[-3:]!='.db']
-[print(c) for c in check]
 
-#%%
-copy, skip, processed = copy_flattened(photos, flat, copy_all='photo')
+# photos = '/Volumes/Toshiba1/PICTURES/PHOTOS/2006'
+# flat = '/Users/vand/Documents/_PHOTOS_FLATTENED/2006'
+# copy, skip, processed = flatten(photos, copy_all='photo')
+
+
+# #%%
+# check = [s for s in skip if s[-3:]!='.db']
+# printlist(check)
+
+# #%%
+# copy, skip, processed = copy_flattened(photos, flat, copy_all='photo')
