bd10a11c247a0a9d7ee748a927976b40b546acc4
Vedrana Andersen Dahl <vedranaa@gmail.com>
2023-04-02 18:10:18 +0200

    adding mesh support to volvizplotly

--- volvizplotly.py
+++ volvizplotly.py
@@ -4,11 +4,11 @@ import plotly.graph_objects as go
 
 
 def grids(dim, i):
-  ''' Returns matrices with coordinates for slicing along axis i. '''
-  two = dim[:i] + dim[i+1:]
-  out = np.mgrid[0:two[0], 0:two[1]]
-  out = np.insert(out, i, np.ones(two), axis=0)
-  return out
+    ''' Returns matrices with coordinates for slicing along axis i. '''
+    two = dim[:i] + dim[i+1:]
+    out = np.mgrid[0:two[0], 0:two[1]]
+    out = np.insert(out, i, np.ones(two), axis=0)
+    return out
 
 
 def volslice(vol, i, s):
@@ -19,59 +19,62 @@ def volslice(vol, i, s):
 
 def volume_slicer(vol, slices, cmin=None, cmax=None, colorscale='Gray',
                   title = '', width=600, height=600):
-  ''' Visualizes slices from volume.
-    vol: a 3D numpy array. 
-    slices: list of 3 list containing slice indices in three directions.
-         Alternatively, each element of slices may be one of the strings
-         'mid', 'ends', 'first' or 'last' or None which is the same as empty.
-  '''
+    ''' Visualizes slices from volume.
+      vol: a 3D numpy array. 
+      slices: list of 3 list containing slice indices in three directions.
+           Alternatively, each element of slices may be one of the strings
+           'mid', 'ends', 'first' or 'last' or None which is the same as empty.
+    '''
   
-  if cmin is None:
-      cmin = vol.min()
-  if cmax is None:
-      cmax = vol.max()
+    if cmin is None:
+        cmin = vol.min()
+    if cmax is None:
+        cmax = vol.max()
       
-  dim = vol.shape
+    dim = vol.shape
 
-  for i in range(len(slices)):
-      sl = slices[i]
-      if type(sl) is not list:
-          if sl=='mid':
-              slices[i] = [dim[i]//2]
-          elif sl=='ends':
-              slices[i] = [0, dim[i]-1]
-          elif sl=='first':
-              slices[i] = [0]
-          elif sl=='last':
-              slices[i] = [dim[i]-1]
-          elif type(sl) is int:
-              slices[i] = [sl]
-          else: # inclusiv None
-              slices[i] = []
+    for i in range(len(slices)):
+        sl = slices[i]
+        if type(sl) is not list:
+            if sl=='mid':
+                slices[i] = [dim[i]//2]
+            elif sl=='ends':
+                slices[i] = [0, dim[i]-1]
+            elif sl=='first':
+                slices[i] = [0]
+            elif sl=='last':
+                slices[i] = [dim[i]-1]
+            elif type(sl) is int:
+                slices[i] = [sl]
+            else: # inclusiv None
+                slices[i] = []
 
-  surfs = []
-  for i in range(3): # three directions
-      gi = grids(dim,i)
-      for j in range(len(slices[i])):
-          g = gi.copy()
-          s = slices[i][j]
-          g[i] *= s
-          surf = dict(x=g[2], y=g[1], z=g[0], surfacecolor=volslice(vol, i, s))
-          surfs.append(surf)
+    surfs = []
+    for i in range(3): # three directions
+        gi = grids(dim,i)
+        for j in range(len(slices[i])):
+            g = gi.copy()
+            s = slices[i][j]
+            g[i] *= s
+            surf = dict(x=g[2], y=g[1], z=g[0], surfacecolor=volslice(vol, i, s))
+            surfs.append(surf)
 
-  common = dict(colorscale=colorscale, cmin=cmin, cmax=cmax)
-  surfaces = [go.Surface({**s, **common}) for s in surfs]
+    common = dict(colorscale=colorscale, cmin=cmin, cmax=cmax)
+    surfaces = [go.Surface({**s, **common}) for s in surfs]
 
-  # Set limits and aspect ratio.
-  d = max(dim)
-  scene = dict(xaxis = dict(range=[-1, dim[2]], autorange=False),
-              yaxis = dict(range=[-1, dim[1]], autorange=False),
-              zaxis = dict(range=[-1, dim[0]], autorange=False), 
-              aspectratio = dict(x=dim[2]/d, y=dim[1]/d, z=dim[0]/d))
-  layout = dict(title=title, width=width, height=height, scene=scene)
+    # Set limits and aspect ratio.
+    d = max(dim)
+    scene = dict(xaxis = dict(range=[-1, dim[2]], autorange=False),
+            yaxis = dict(range=[-1, dim[1]], autorange=False),
+            zaxis = dict(range=[-1, dim[0]], autorange=False), 
+            aspectratio = dict(x=dim[2]/d, y=dim[1]/d, z=dim[0]/d))
+    layout = dict(title=title, width=width, height=height, scene=scene)
 
-  fig = go.Figure(data=surfaces, layout=layout)
-  fig.show()
+    fig = go.Figure()
+    fig.add_traces(surfaces)
+    fig.update_layout(layout)  
+    # fig = go.Figure(data=surfaces, layout=layout)
+    fig.show()
 
 
 def interactive_volume_slicer(vol, cmin=None, cmax=None, colorscale='Gray',
@@ -170,4 +173,51 @@ def interactive_volume_slicer(vol, cmin=None, cmax=None, colorscale='Gray',
             sliders = [slider_dict]
     )
 
+    fig.show()
+
+
+def pointcloud_plot(points):
+
+    gm = go.Scatter3d(z=points[:,0], y=points[:,1], x=points[:,2], 
+                      mode='markers', name='')
+    return gm 
+
+
+def mesh_surface_plot(vertices, faces):
+
+    gm = go.Mesh3d(z=vertices[:,0], y=vertices[:,1], x=vertices[:,2], 
+            i=faces[:,0], j=faces[:,1], k=faces[:,2])
+    return gm
+
+
+def mesh_wireframe_plot(vertices, faces):
+    
+    Xe = np.concatenate((vertices[faces, 0], np.full((faces.shape[0],1), None)),
+                        axis=1).ravel()
+    Ye = np.concatenate((vertices[faces, 1], np.full((faces.shape[0],1), None)),
+                        axis=1).ravel()
+    Ze = np.concatenate((vertices[faces, 2], np.full((faces.shape[0],1), None)),
+                        axis=1).ravel()
+    
+    gm = go.Scatter3d(z=Xe, y=Ye, x=Ze, mode='lines', name='',
+            line=dict(color= 'rgb(40,40,40)', width=1))  
+    
+    return gm
+
+
+def show_mesh(vertices, faces=None, wireframe=False, surf=True):
+
+    fig = go.Figure()
+  
+    if surf and faces: 
+        fig.add_trace(mesh_surface_plot(vertices, faces))
+  
+    if wireframe and faces:        
+        fig.add_trace(mesh_wireframe_plot(vertices, faces))
+  
+    if ((not surf) and (not wireframe)) or (not faces):
+        fig.add_trace(pointcloud_plot(vertices))
+        fig.update_traces(marker_size = 1)
+    #fig.update_layout(title_text='Mesh', height=600, width=600)
+  
     fig.show()
